94d892c6e98cca44d13d02e92d13d44b2339e581
Nick Swanevelder <nickyswa@gmail.com>
2018-09-13 22:33:54 +0200

    Initial commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,25 @@
+//Scroll right function
+
+(function() {
+    function scrollHorizontally(e) {
+        e = window.event || e;
+        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+        document.getElementById('scroll').scrollLeft -= (delta*90); // Multiplied by 40
+        e.preventDefault();
+    }
+    if (document.getElementById('scroll').addEventListener) {
+        // IE9, Chrome, Safari, Opera
+        document.getElementById('scroll').addEventListener("mousewheel", scrollHorizontally, false);
+        // Firefox
+        document.getElementById('scroll').addEventListener("DOMMouseScroll", scrollHorizontally, false);
+    } else {
+        // IE 6/7/8
+        document.getElementById('scroll').attachEvent("onmousewheel", scrollHorizontally);
+    }
+})();
+
+//On load fade in function
+
+$(document).ready(function() {
+    $("#scroll").hide().fadeIn(2000);
+});
